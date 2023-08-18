59b7f812d235ae38eaa583822b8dfb774d1c5481
Henrique Almeida <ihenriquexo@gmail.com>
2023-08-17 21:42:27 -0300

    dia 3 final

--- dia 3.js
+++ dia 3.js
@@ -1,6 +1,11 @@
-//let e const
+//let entradaDoUsuario = parseInt(prompt("entrada do usurário"))
+//console.log(entradaDoUsuario)
 
-let meuNome = "henrique"
-console.log(meuNome)
-meuNome = "Almeida"
-console.log(meuNome)
+//lição dia 3
+let nome=prompt("insira seu nome")
+let idade=parseInt(prompt("idade"))
+let altura=prompt("altura")
+let peso=prompt("peso")
+let nascimento=2023-idade
+let imc=peso/(altura*altura)
+console.log("Olá "+nome+", você tem "+idade+" anos, nasceu em "+nascimento+", tem "+altura+" de altura, pesa"+peso+" kg, seu imc é "+imc+" kg/m2")
